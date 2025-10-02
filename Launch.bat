2f4ee79eccef0690f687395d5d11a6a85b9953d1
Vladimir Skop <v.skop@mail.ru>
2025-10-02 11:47:24 +0300

    Changed Launch.bat

--- Launch.bat
+++ Launch.bat
@@ -1,3 +1,7 @@
 @echo off
+cd
+curl -O -L https://github.com/vlskop/daily_planner/archive/refs/heads/main.zip
+curl -O -L https://github.com/vlskop/daily_planner/blob/main/Launch.bat
+tar -xf main.zip
 echo Starting Planner Application...
-start msedge --app="file:///%CD%/index.html"
+start msedge --app="file:///%CD%/daily_planner-main/index.html"
